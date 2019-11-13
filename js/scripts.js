0f7a05ce1dfe75d990917f2832cb2800bdc1a284
ian mwangi <iankimotho233@gmail.com>
2019-11-13 14:38:27 +0300

    Prevent voters from voting more than one presidential candidate

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    $("button#election").click(function(){
+    var presidentVote1 = 0;
+    var presidentVote2 = 0;
+
+    $("button#election").one("click",function(){
         $(".chooseCounty").show();
     });
     $("button#submit-county").click(function(){
@@ -9,5 +12,17 @@ $(document).ready(function(){
             $("#but1").text("Vote");
             $("#but2").text("Vote");
         }
+    });
+    $("#Candidate1").one("click",function(){
+        presidentVote1+=1;
+        $("#but1").text("+1");
+        $("#Candidate2").attr("disabled",true);
+        alert(presidentVote1);
+    });
+    $("#Candidate2").one("click",function(){
+        presidentVote2+=1;
+        $("#but2").text("+1");
+        $("#Candidate1").attr("disabled",true);
+        alert(presidentVote2);
     });
 });
