dbb126ad3c160f60d9cc442488a12f0146704ee6
ian mwangi <iankimotho233@gmail.com>
2019-11-14 08:50:15 +0300

    Add the section to vote for the governor

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
     var presidentVote1 = 0;
     var presidentVote2 = 0;
+    var governorVote1 = 0;
+    var governorVote2 = 0;
 
     $("button#election").one("click",function(){
+        $(".details").hide();
         $(".chooseCounty").show();
     });
     $("button#submit-county").click(function(){
-        if($("#county").val()==="nairobi"){
+        if($("#county").val()==="nairobi" || $("#county").val()==="kiambu"){
             $(".chooseCounty").hide();
-            $(".votingPage").show();
+            $(".prezzo").show();
+            $(".nameOfCounty").text($("#county").val());
+            $(".governor").show();
             $("#but1").text("Vote");
             $("#but2").text("Vote");
+            $("#but3").text("Vote");
+            $("#but4").text("Vote");
         }
     });
     $("#Candidate1").one("click",function(){
@@ -25,4 +32,17 @@ $(document).ready(function(){
         $("#Candidate1").attr("disabled",true);
         alert(presidentVote2);
     });
+    $("#Candidate3").one("click",function(){
+        governorVote1+=1;
+        $("#but3").text("+1");
+        $("#Candidate4").attr("disabled",true);
+        alert(governorVote1);
+    });
+    $("#Candidate4").one("click",function(){
+        governorVote2+=1;
+        $("#but4").text("+1");
+        $("#Candidate3").attr("disabled",true);
+        alert(governorVote2);
+    });
+    
 });
