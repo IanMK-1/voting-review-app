a4ce6b03f60a84b64b7e65bc7fc4d74ddd8296b6
ian mwangi <iankimotho233@gmail.com>
2019-11-15 10:13:25 +0300

    Add the review section

---
+++ js/review.js
@@ -0,0 +1,521 @@
+
+$( document).ready(function(){
+    $(".can").click(function(event){
+    $(".candidates option:selected").show("#mp");
+        let candidate = $(".candidates option:selected").val();
+    });
+});
+$(document).ready(function(){
+
+    // $(".candidates").change(function(){
+    //     // if ($(this).val() =="#president") {
+    //     //     window.location = "#"+ this.val();
+    //     // }
+    //     console.log("b");
+    // }
+    $(".candidates").change(function(){
+        if ($(this).val() === "president" || "governor" || "mp" || "mca"  ) {
+             window.location = "#"+ $(this).val();
+            console.log($(this).val());
+             }
+
+    })
+})
+$(document).ready(function(){
+ $("#s0").click(function(){
+     $("#s0").css("color","yellow");
+     
+ })   
+ $("#s1").click(function(){
+    $("#s1").css("color","yellow");
+    
+})
+    $("#s2").click(function(){
+    $("#s2").css("color","yellow");
+    
+})
+    $("#s3").click(function(){
+    $("#s3").css("color","yellow");
+    
+})
+    $("#s4").click(function(){
+    $("#s4").css("color","yellow");
+    
+})   
+ $("#s5").click(function(){
+    $("#s5").css("color","yellow");
+    
+})    
+$("#s6").click(function(){
+    $("#s6").css("color","yellow");
+    
+})    
+$("#s7").click(function(){
+    $("#s7").css("color","yellow");
+    
+})    
+$("#s8").click(function(){
+    $("#s8").css("color","yellow");
+    
+})    
+$("#s9").click(function(){
+    $("#s9").css("color","yellow");
+    
+})    
+$("#s10").click(function(){
+    $("#s10").css("color","yellow");
+    
+}) 
+   $("#s11").click(function(){
+    $("#s11").css("color","yellow");
+    
+}) 
+   $("#s12").click(function(){
+    $("#s12").css("color","yellow");
+    
+})    
+$("#s13").click(function(){
+    $("#s13").css("color","yellow");
+    
+})    
+$("#s14").click(function(){
+    $("#s14").css("color","yellow");
+    
+})    
+$("#s15").click(function(){
+    $("#s15").css("color","yellow");
+    
+})   
+ $("#s16").click(function(){
+    $("#s16").css("color","yellow");
+    
+})    
+$("#s17").click(function(){
+    $("#s17").css("color","yellow");
+    
+})    
+$("#s18").click(function(){
+    $("#s18").css("color","yellow");
+    
+})    
+$("#s19").click(function(){
+    $("#s19").css("color","yellow");
+    
+})    
+$("#s20").click(function(){
+    $("#s20").css("color","yellow");
+    
+})    
+$("#s21").click(function(){
+    $("#s21").css("color","yellow");
+    
+})    
+$("#s22").click(function(){
+    $("#s22").css("color","yellow");
+    
+})    
+$("#s23").click(function(){
+    $("#s23").css("color","yellow");
+    
+})    
+$("#s24").click(function(){
+    $("#s24").css("color","yellow");
+    
+})    
+$("#s25").click(function(){
+    $("#s25").css("color","yellow");
+    
+})    
+$("#s26").click(function(){
+    $("#s26").css("color","yellow");
+    
+})   
+ $("#s27").click(function(){
+    $("#s27").css("color","yellow");
+    
+})  
+  $("#s28").click(function(){
+    $("#s28").css("color","yellow");
+    
+})    
+$("#s29").click(function(){
+    $("#s29").css("color","yellow");
+    
+})    
+$("#s30").click(function(){
+    $("#s30").css("color","yellow");
+    
+})    
+$("#s31").click(function(){
+    $("#s31").css("color","yellow");
+    
+})   
+$("#s32").click(function(){
+    $("#s32").css("color","yellow");
+    
+})   
+$("#s33").click(function(){
+    $("#s33").css("color","yellow");
+    
+})   
+$("#s34").click(function(){
+    $("#s34").css("color","yellow");
+    
+})   
+$("#s35").click(function(){
+    $("#s35").css("color","yellow");
+    
+})   
+$("#s36").click(function(){
+    $("#s36").css("color","yellow");
+    
+})   
+$("#s37").click(function(){
+    $("#s37").css("color","yellow");
+    
+})   
+$("#s38").click(function(){
+    $("#s38").css("color","yellow");
+    
+})   
+$("#s39").click(function(){
+    $("#s39").css("color","yellow");
+    
+})   
+$("#s41").click(function(){
+    $("#s41").css("color","yellow");
+    
+})   
+$("#s42").click(function(){
+    $("#s42").css("color","yellow");
+    
+})   
+$("#s43").click(function(){
+    $("#s43").css("color","yellow");
+    
+})   
+$("#s44").click(function(){
+    $("#s44").css("color","yellow");
+    
+})   
+$("#s45").click(function(){
+    $("#s45").css("color","yellow");
+    
+})   
+$("#s46").click(function(){
+    $("#s46").css("color","yellow");
+    
+})   
+$("#s47").click(function(){
+    $("#s47").css("color","yellow");
+    
+})   
+$("#s48").click(function(){
+    $("#s48").css("color","yellow");
+    
+})   
+$("#s49").click(function(){
+    $("#s49").css("color","yellow");
+    
+})   
+$("#s51").click(function(){
+    $("#s51").css("color","yellow");
+    
+})   
+$("#s52").click(function(){
+    $("#s52").css("color","yellow");
+    
+})   
+$("#s53").click(function(){
+    $("#s53").css("color","yellow");
+    
+})   
+$("#s54").click(function(){
+    $("#s54").css("color","yellow");
+    
+})   
+$("#s55").click(function(){
+    $("#s55").css("color","yellow");
+    
+})   
+$("#s56").click(function(){
+    $("#s56").css("color","yellow");
+    
+})   
+$("#s57").click(function(){
+    $("#s57").css("color","yellow");
+    
+})   
+$("#s58").click(function(){
+    $("#s58").css("color","yellow");
+    
+})   
+$("#s59").click(function(){
+    $("#s59").css("color","yellow");
+    
+})   
+$("#s60").click(function(){
+    $("#s60").css("color","yellow");
+    
+})   
+$("#s61").click(function(){
+    $("#s61").css("color","yellow");
+    
+})   
+$("#s62").click(function(){
+    $("#s62").css("color","yellow");
+    
+})   
+$("#s63").click(function(){
+    $("#s63").css("color","yellow");
+    
+})   
+$("#s64").click(function(){
+    $("#s64").css("color","yellow");
+    
+})   
+$("#s65").click(function(){
+    $("#s65").css("color","yellow");
+    
+})   
+$("#s66").click(function(){
+    $("#s66").css("color","yellow");
+    
+})   
+$("#s67").click(function(){
+    $("#s67").css("color","yellow");
+    
+})   
+$("#s68").click(function(){
+    $("#s68").css("color","yellow");
+    
+})   
+$("#s69").click(function(){
+    $("#s69").css("color","yellow");
+    
+})   
+$("#s70").click(function(){
+    $("#s70").css("color","yellow");
+    
+})   
+$("#s71").click(function(){
+    $("#s71").css("color","yellow");
+    
+})   
+$("#s72").click(function(){
+    $("#s72").css("color","yellow");
+    
+})   
+$("#s73").click(function(){
+    $("#s73").css("color","yellow");
+    
+})   
+$("#s74").click(function(){
+    $("#s74").css("color","yellow");
+    
+})   
+$("#s75").click(function(){
+    $("#s75").css("color","yellow");
+    
+})   
+$("#s76").click(function(){
+    $("#s76").css("color","yellow");
+    
+})   
+$("#s77").click(function(){
+    $("#s77").css("color","yellow");
+    
+})   
+$("#s78").click(function(){
+    $("#s78").css("color","yellow");
+    
+})   
+$("#s79").click(function(){
+    $("#s79").css("color","yellow");
+    
+})   
+$("#s80").click(function(){
+    $("#s80").css("color","yellow");
+    
+})   
+$("#s80").click(function(){
+    $("#s80").css("color","yellow");
+    
+})   
+$("#s80").click(function(){
+    $("#s80").css("color","yellow");
+    
+})   
+$("#s81").click(function(){
+    $("#s81").css("color","yellow");
+    
+})   
+$("#s82").click(function(){
+    $("#s82").css("color","yellow");
+    
+})   
+$("#s83").click(function(){
+    $("#s83").css("color","yellow");
+    
+})   
+$("#s84").click(function(){
+    $("#s84").css("color","yellow");
+    
+})   
+$("#s85").click(function(){
+    $("#s85").css("color","yellow");
+    
+})   
+$("#s86").click(function(){
+    $("#s86").css("color","yellow");
+    
+})   
+$("#s87").click(function(){
+    $("#s87").css("color","yellow");
+    
+})   
+$("#s88").click(function(){
+    $("#s88").css("color","yellow");
+    
+})   
+$("#s89").click(function(){
+    $("#s89").css("color","yellow");
+    
+})   
+$("#s90").click(function(){
+    $("#s90").css("color","yellow");
+    
+})   
+$("#s91").click(function(){
+    $("#s91").css("color","yellow");
+    
+})   
+$("#s92").click(function(){
+    $("#s92").css("color","yellow");
+    
+})   
+$("#s93").click(function(){
+    $("#s93").css("color","yellow");
+    
+})   
+$("#s94").click(function(){
+    $("#s94").css("color","yellow");
+    
+})   
+$("#s95").click(function(){
+    $("#s95").css("color","yellow");
+    
+})   
+$("#s96").click(function(){
+    $("#s96").css("color","yellow");
+    
+})   
+$("#s97").click(function(){
+    $("#s97").css("color","yellow");
+    
+})   
+$("#s98").click(function(){
+    $("#s98").css("color","yellow");
+    
+})   
+$("#s99").click(function(){
+    $("#s99").css("color","yellow");
+    
+})   
+$("#s100").click(function(){
+    $("#s100").css("color","yellow");
+    
+})   
+$("#s101").click(function(){
+    $("#s101").css("color","yellow");
+    
+})   
+$("#s102").click(function(){
+    $("#s102").css("color","yellow");
+    
+})   
+$("#s103").click(function(){
+    $("#s103").css("color","yellow");
+    
+})   
+$("#s104").click(function(){
+    $("#s104").css("color","yellow");
+    
+})   
+$("#s104").click(function(){
+    $("#s104").css("color","yellow");
+    
+})   
+$("#s105").click(function(){
+    $("#s105").css("color","yellow");
+    
+})   
+$("#s106").click(function(){
+    $("#s106").css("color","yellow");
+    
+})   
+$("#s107").click(function(){
+    $("#s107").css("color","yellow");
+    
+})   
+$("#s108").click(function(){
+    $("#s108").css("color","yellow");
+    
+})   
+
+
+$("#s109").click(function(){
+    $("#s109").css("color","yellow");
+    
+})   
+
+$("#s110").click(function(){
+    $("#s110").css("color","yellow");
+    
+})   
+
+$("#s111").click(function(){
+    $("#s111").css("color","yellow");
+    
+})   
+$("#s112").click(function(){
+    $("#s112").css("color","yellow");
+    
+})   
+$("#s113").click(function(){
+    $("#s113").css("color","yellow");
+    
+})   
+$("#s114").click(function(){
+    $("#s114").css("color","yellow");
+    
+})   
+$("#s115").click(function(){
+    $("#s115").css("color","yellow");
+    
+})   
+$("#s116").click(function(){
+    $("#s116").css("color","yellow");
+    
+})   
+$("#s117").click(function(){
+    $("#s117").css("color","yellow");
+    
+})   
+$("#s118").click(function(){
+    $("#s118").css("color","yellow");
+    
+})   
+})
+$('#submit').click(function () {
+   
+    var Message = $('#message');
+    event.preventDefault();
+    if ( Message == '' ){
+      alert('Please make sure you have filled in correctly ');
+    } else {
+      alert (  'we have received your mesage. Thank you for giving us our feedback.');
+    }
+    
+    });
+    
